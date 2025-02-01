5d24636c302c6621cab9c796e5d227a8abd1e3ae
Troytdev <troy_teelucksingh97@hotmail.com>
2025-02-01 04:05:18 +0000

    testing modal close

--- assets/js/main.js
+++ assets/js/main.js
@@ -298,6 +298,13 @@
 							location.hash = '';
 						});
 
+			    // Modal Close.
+					$('<div class="modal-close">Close</div>')
+					.appendTo($this)
+					.on('click', function() {
+						location.hash = '#Projects';
+					});
+
 				// Prevent clicks from inside article from bubbling.
 					$this.on('click', function(event) {
 						event.stopPropagation();
