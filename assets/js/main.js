9120f306c8869021cac86240bd0e87a75752d31c
Troytdev <troy_teelucksingh97@hotmail.com>
2025-02-01 16:48:53 +0000

    testing modal close

--- assets/js/main.js
+++ assets/js/main.js
@@ -292,11 +292,11 @@
 				var $this = $(this);
 
 				// Close.
-					$('<div class="close">Close</div>')
+					/*$('<div class="close">Close</div>')
 						.appendTo($this)
 						.on('click', function() {
 							location.hash = '';
-						});
+						});*/
 
 			    // Modal Close.
 					$('<div class="modal-close">Close</div>')
